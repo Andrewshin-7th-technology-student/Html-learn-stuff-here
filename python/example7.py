e13e20589186f201f41764a1297e06265afd7a4c
Andrewshin-7th-technology-student <480132@bsd48.org>
2024-11-06 14:45:46 -0800

    Create example7.py

---
+++ python/example7.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+
+def on_button_click():
+    print("Button clicked!")
+
+# Create the main window
+root = tk.Tk()
+root.title("Simple GUI")
+
+# Create a button
+button = tk.Button(root, text="Click Me", command=on_button_click)
+button.pack()
+
+# Run the application
+root.mainloop()
